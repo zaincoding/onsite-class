6f99fda178a996be88252bfdbc78f71c2b1216cb
zain <zainmd.htm@gmail.com>
2024-05-02 18:51:11 +0500

    Add files

---
+++ Object & it's type/main.js
@@ -0,0 +1,17 @@
+// let student = {
+//     name: 'shoiab',
+//     age: 25,
+//     gender: 'male',
+//     email: 'shoaib@gmail.com',
+//     dob: 2000
+// }
+//let modelNumber: ModelNumber = true;
+var myCar = {
+    name: 'Civic',
+    color: 'white',
+    modelNumber: true,
+};
+myCar.color = 'black';
+console.log(myCar.modelNumber);
+var Username = "abc123";
+console.log(Username);
---
+++ Object & it's type/main.ts
@@ -0,0 +1,111 @@
+// let student = {
+//     name: 'shoiab',
+//     age: 25,
+//     gender: 'male',
+//     email: 'shoaib@gmail.com',
+//     dob: 2000
+// }
+
+// console.log(`My name is ${student}`);
+// console.log(student)
+
+
+// console.log(`My name is ${student.name}`);
+// console.log(student.name)
+
+// let car: {
+//     name: string,
+//     color: string,
+//     horsePower: string,
+//     model: number
+// } = {
+//     name: 'Toyota',
+//     color: 'white',
+//     horsePower: '1000cc',
+//     model: 2025
+// }
+
+
+
+
+
+
+
+
+// let student:{name:string,age:number,email:string} = {
+     
+//     name:'Usman',
+//     age: 30,
+//     email: 'Usman@gmail.com'
+// }
+
+// // console.log('My age is ${student}')
+// // console.log(student);
+// console.log(student.age)
+
+
+
+// let leptop:{brand:string, price:number,screenSize:string,color:string} = {
+
+//     brand: 'Dell',
+//     price: 25000,
+//     screenSize: '14inch',
+//     color:'white'
+// }
+
+// // console.log(leptop);
+// console.log(leptop.brand)
+
+
+// type employee ={
+//    name:string,
+//    age:number,
+//    isAdmin:boolean
+// }
+
+// let employee1:employee ={
+//     name:"Usman",
+//     age:25,
+//     isAdmin: true
+// }
+
+// let employee2:employee ={
+//     name:"Amir",
+//     age:30,
+//     isAdmin: false
+// }
+
+
+// type ModelNumber = number | string | boolean;
+// type car ={
+//     name:string
+//     color:string
+//   //  modelNumber:number
+//     modelNumber:ModelNumber
+//     powerStering?:boolean
+
+// }
+
+//let modelNumber: ModelNumber = true;
+
+// let myCar:car = {
+//     name:'Civic',
+//     color:'white',
+//     modelNumber: true,
+// }
+// myCar.color = 'black';
+
+// console.log(myCar.modelNumber)
+
+
+
+//    type Username = 'Amir'
+
+//    let name: Username: 'Ali'
+
+type Faculty = "Student" | "Admin" | "Teacher"| "TA" | "abc123"
+
+let Username:Faculty = "abc123"
+
+
+console.log(Username)
