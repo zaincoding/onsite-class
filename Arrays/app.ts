9a6df9c0d800368df5ebf162bcbc89109c60e27b
zain <zainmd.htm@gmail.com>
2024-05-16 19:30:23 +0500

    Add Array files

---
+++ Arrays/app.ts
@@ -0,0 +1,81 @@
+// let  student = ["Hamza Alvi","Ameen Alam", "Fawad Khan"];
+
+//  console.log(student);
+
+//  let numbers = [1,2,3,4,5,6];
+
+//  console.log(numbers);
+
+//  let booleanArray = [true,false,true, false]
+
+//  console.log(booleanArray)
+
+//  let mixedArray = ["Hamza Alvi",1454,true,"Ameen Alam"];
+
+//   console.log(mixedArray)
+ 
+
+// let newArray:string[]=['Maria','Hamza'];
+// let numberNew:number[] =[1,2,3,4,5];
+
+// let arrayU:(string|number|boolean)[]=['hello',2,'world',4,true];
+
+
+// console.log(newArray[1])
+
+
+//3,4 index required
+
+// let student:string[] = ['student1','student2','student3','student4','student5'];
+
+// console.log(student[2]);
+// console.log(student[3]);
+
+// let teacher:string[] =['maria','Hamza'];
+
+// console.log(teacher);
+
+// teacher[1] = "Aziz";
+
+
+// let student:string[] = ['Amir','Zahid','Hamza','jawad','sohil'];
+
+// console.log(student)
+// student[2] = "Aziz";
+// student.push('zia','Amjed');
+// student.push('anwar')
+// console.log(student)
+
+// let student:string[] = ['Amir','Zahid','Hamza','jawad','sohil'];
+// console.log(student);
+// student.pop();
+// student.pop();
+// console.log(student);
+
+// let student:string[] = ['Amir','Zahid','Hamza','jawad','sohil'];
+// console.log(student);
+// student.unshift('Umair','Aziz')
+// console.log(student);
+
+
+// let student:string[] = ['Amir','Zahid','Hamza','jawad','sohil'];
+// console.log(student);
+// student.shift()
+// console.log(student);
+
+let student:string[] = ['Amir','Zahid','Hamza','jawad','sohil'];
+
+console.log(student)
+
+student.unshift('Amjeed',"Ali")
+
+console.log(student)
+
+student.shift()
+student.shift()
+console.log(student);
+
+
+
+
+
--- Assignment2/Question1/app.js
+++ Assignment2/Question1/app.js
@@ -1,8 +1,4 @@
 //Assignment Q1:
-var miles = 5;
-var kilometers = miles * 1.60934;
+let miles = 5;
+let kilometers = miles * 1.60934;
 console.log("The distance ".concat(kilometers, " kms is equal to ").concat(miles, " miles"));
-//Assignment Q2:
-var userNumber = Number(prompt("Enter number", ''));
-var randomNumber = Math.random() * 3 + 1;
-console.log(randomNumber);
