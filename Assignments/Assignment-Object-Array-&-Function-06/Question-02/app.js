15891cf01392cba909c8382851a30c8ff166ff77
zain <zainmd.htm@gmail.com>
2024-05-30 18:34:26 +0500

    Add files

--- Assignments/Assignment-Object-Array-&-Function-06/Question-02/app.js
+++ Assignments/Assignment-Object-Array-&-Function-06/Question-02/app.js
@@ -1,11 +1,10 @@
 var scrambledArray = ["student", "of", true, 123, "am", "a", "GIAIC", "I"];
 var convert = scrambledArray[3].toString();
-console.log(convert);
 scrambledArray.splice(3, 1, convert);
-console.log("Value of boolean converted to, \"".concat(typeof convert, "\""));
+console.log("Value of number converted to, \"".concat(typeof convert, "\""));
 var convert2 = scrambledArray[2].toString();
 scrambledArray.splice(2, 1, convert2);
-console.log("Value of number converted to, \"".concat(typeof convert2, "\""));
+console.log("Value of boolean converted to, \"".concat(typeof convert2, "\""));
 console.log(scrambledArray);
 scrambledArray.splice(0, 4);
 scrambledArray.pop();
---
+++ C06.loops/loop.js
@@ -0,0 +1,57 @@
+//initial condition
+//stopic condition.
+//updation or incremental statement
+// for(let count=3; count<=10; count++){
+//     console.log("Hello world")
+//  }
+// for(let count=3; count<=10; count++){
+//    // console.log("Hello world")
+//    console.log(count +"Count:")
+//  }
+//make a function, which takes a parameter it type number.
+//and in function block write a for loop.
+//which  print the number table and.
+//  const printTable = (num:number) =>{
+//     for(let i =1; i<=10; i++){
+//         console.log(`${num} * ${i} = ${num*i}`)
+//     }
+//  }
+//  console.log(printTable(10))
+// const sumeofN = (num:number) =>{
+//     let sum=0;
+//     for(let j =1; j<=num; j++){
+//         sum  =sum+j;
+//     }
+//     console.log(sum)
+//  }
+//  sumeofN(5)
+// function numb(num:number){
+//     let i =1;
+// while(i<=10){
+//    let table =(`${num * i} = ${num * i}`);
+//    console.log(table)
+//     i++;
+// }
+// }
+//for in is mostly used for objects
+// let student ={
+//     name: "Hamza",
+//     batch: "Thursday class",
+//     isAvailable: false,
+// };
+// for(let x in student){
+//     console.log(x, "X")
+// }
+// //use to access statical property.
+// console.log(obj.name);
+// //use to access property dynamically.
+// console.log(obj["name"]);
+//numb(2)
+var student = {
+    name: "Hamza",
+    batch: "Thursday class",
+    isAvailable: false,
+};
+for (var x in student) {
+    console.log(x, student[x]);
+}
